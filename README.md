3dddbd166a056c7907bacd4f6536f21f5b9a2561
Tao Wen <taowen@gmail.com>
2016-12-06 21:40:24 +0800

    medium benchmark

--- README.md
+++ README.md
@@ -5,11 +5,29 @@ test file used: https://github.com/json-iterator/test-data/blob/master/large-fil
 * jsoniter reflect-api: https://github.com/json-iterator/go/blob/master/jsoniter_reflect.go
 * encoding/json: golang standard lib
 
+The goal is to prove jsoniter is not slow, not to prove it is the fastest, at least good enough.
+My motivation of inventing json iterator is the flexibility to mix high level and low level api.
+Performance is the by-product of schema based parsing.
+
+* CPU: i7-4790 @ 3.6G
+* Go: 1.8beta1
+
 # small payload
 
 | jsonparser  | jsoniter pull-api | jsoniter reflect-api | encoding/json |
-| --          | --                | --                   | --            |
-| 691 ns/op   | 661 ns/op         | 982 ns/op            | 4740 ns/op	  |
+| ---         | ---               | ---                  | ---           |
+| 691 ns/op   | 661 ns/op         | 982 ns/op            | 4740 ns/op    |
 | 64 B/op     | 64 B/op           | 256 B/op             | 864 B/op      |
 | 2 allocs/op | 2 allocs/op       | 4 allocs/op          | 31 allocs/op  |
 
+pull-api is fast, and reflect-api is not slow either.
+
+# medium payload
+
+| jsonparser  | jsoniter pull-api | jsoniter reflect-api | encoding/json |
+| ---         | ---               | ---                  | ---           |
+| 5958 ns/op  | 5662 ns/op        | 6943 ns/op           | 30902 ns/op   |
+| 104 B/op    | 104 B/op          | 344 B/op             | 2168 B/op     |
+| 4 allocs/op | 4 allocs/op       | 14 allocs/op         | 107 allocs/op |
+
+again, the reflect-api is not slow
---
+++ src/github.com/json-iterator/go-benchmark/benchmark_medium_payload_test.go
@@ -0,0 +1,142 @@
+package go_benchmark
+
+import (
+	"github.com/buger/jsonparser"
+	"testing"
+	"github.com/json-iterator/go"
+	"encoding/json"
+)
+
+func BenchmarkJsonParserEachKeyStructMedium(b *testing.B) {
+	paths := [][]string{
+		[]string{"person", "name", "fullName"},
+		[]string{"person", "github", "followers"},
+		[]string{"company"},
+		[]string{"person", "gravatar", "avatars"},
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		data := MediumPayload{
+			Person: &CBPerson{
+				Name:     &CBName{},
+				Github:   &CBGithub{},
+				Gravatar: &CBGravatar{},
+			},
+		}
+
+		jsonparser.EachKey(mediumFixture, func(idx int, value []byte, vt jsonparser.ValueType, err error) {
+			switch idx {
+			case 0:
+				data.Person.Name.FullName, _ = jsonparser.ParseString(value)
+			case 1:
+				v, _ := jsonparser.ParseInt(value)
+				data.Person.Github.Followers = int(v)
+			case 2:
+				v, _ := jsonparser.ParseString(value)
+				data.Company = v
+			case 3:
+				var avatars []*CBAvatar
+				jsonparser.ArrayEach(value, func(avalue []byte, dataType jsonparser.ValueType, offset int, err error) {
+					url, _ := jsonparser.GetString(avalue, "url")
+					avatars = append(avatars, &CBAvatar{Url: url})
+				})
+				data.Person.Gravatar.Avatars = avatars
+			}
+		}, paths...)
+	}
+}
+
+func BenchmarkJsoniterStructMedium(b *testing.B) {
+	iter := jsoniter.ParseBytes(mediumFixture)
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		data := MediumPayload{
+			Person: &CBPerson{
+				Name:     &CBName{},
+				Github:   &CBGithub{},
+				Gravatar: &CBGravatar{},
+			},
+		}
+		iter.Reuse(mediumFixture)
+		for field := iter.ReadObject(); field != ""; field = iter.ReadObject() {
+			switch field {
+			case "company":
+				data.Company = iter.ReadString()
+			case "person":
+				for field := iter.ReadObject(); field != ""; field = iter.ReadObject() {
+					switch field {
+					case "name":
+						for field := iter.ReadObject(); field != ""; field = iter.ReadObject() {
+							if "fullName" != field {
+								iter.Skip()
+								continue
+							}
+							data.Person.Name.FullName = iter.ReadString()
+						}
+					case "github":
+						for field := iter.ReadObject(); field != ""; field = iter.ReadObject() {
+							if "followers" != field {
+								iter.Skip()
+								continue
+							}
+							data.Person.Github.Followers = iter.ReadInt()
+						}
+					case "gravatar":
+						for field := iter.ReadObject(); field != ""; field = iter.ReadObject() {
+							if "avatars" != field {
+								iter.Skip()
+								continue
+							}
+							var avatars []*CBAvatar
+							for iter.ReadArray() {
+								for field := iter.ReadObject(); field != ""; field = iter.ReadObject() {
+									if "url" != field {
+										iter.Skip()
+										continue
+									}
+									avatars = append(avatars, &CBAvatar{Url: iter.ReadString()})
+								}
+							}
+							data.Person.Gravatar.Avatars = avatars
+						}
+					default:
+						iter.Skip()
+					}
+				}
+			default:
+				iter.Skip()
+			}
+		}
+	}
+}
+
+
+func BenchmarkJsoniterReflectStructMedium(b *testing.B) {
+	iter := jsoniter.ParseBytes(mediumFixture)
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		data := MediumPayload{
+			Person: &CBPerson{
+				Name:     &CBName{},
+				Github:   &CBGithub{},
+				Gravatar: &CBGravatar{},
+			},
+		}
+		iter.Reuse(mediumFixture)
+		iter.Read(&data)
+	}
+}
+
+/*
+   encoding/json
+*/
+func BenchmarkEncodingJsonStructMedium(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		var data MediumPayload
+		json.Unmarshal(mediumFixture, &data)
+	}
+}
